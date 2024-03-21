357984b4912192e98d46815fe16d8a6b9fda5546
prateekpandey-bnt <prateek.pandey@bnt-soft.com>
2024-03-21 12:20:35 +0530

    small changes

--- src/main/java/com/bnt/githubaction/GithubActionDemoApplication.java
+++ src/main/java/com/bnt/githubaction/GithubActionDemoApplication.java
@@ -2,12 +2,21 @@ package com.bnt.githubaction;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class GithubActionDemoApplication {
-
+	
+	@GetMapping("/welcome")
+	public String welcome() {
+		return "This is github action demo project!!";
+		
+	}
 	public static void main(String[] args) {
 		SpringApplication.run(GithubActionDemoApplication.class, args);
+		
 	}
 
 }
